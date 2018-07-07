82ce97dc895109b8cbb9dd58b42fc13d74be2d75
Larisa Dolgova <larisa.dolgova@futurice.com>
2018-07-07 16:39:38 +0200

    Update query

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+const countryList = [
+  {
+    name: "Italy",
+    population: 10
+  },
+  {
+    name: "France",
+    population: 20
+  },
+  {
+    name: "England",
+    population: 30
+  },
+  {
+    name: "Germany",
+    population: 40
+  }
+]
+
 const typeDefs = `
-  type Query {
-    hello(name: String): String!
+ type Query {
+    countries(min: Int, max: Int): [String!]!
   }
 `
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello, ${name || 'World'}`,
+    countries: (_, { min, max }) => countryList
+      .filter(country => country.population > min && country.population < max)
+      .map(country => country.name),
   },
 }
 
